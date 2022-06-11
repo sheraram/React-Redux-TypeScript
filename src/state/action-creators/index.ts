8b1fa4440c5624b3497b2ea9bec29a415e4db776
SHERA RAM <sheraram998202@gmail.com>
2022-06-11 23:00:24 +0530

    ActionCreators

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,29 @@
+import { Dispatch } from "redux";
+import { ActionType } from "../action-type"
+import { Action } from "../actions";
+
+export const depositMoney = (amount: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.DEPOSIT,
+            payload: amount
+        })
+    }
+};
+
+export const withdrawMoney = (amount: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.WITHDRAW,
+            payload: amount
+        })
+    }
+};
+
+export const bankrupt = (amount: number) => {
+    return (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.BANKRUPT
+        })
+    }
+};
